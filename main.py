f6366d86b74c619bf17cc34d6ab109b739d76d6a
Zhusha7 <andzej200@gmail.com>
2025-01-06 00:42:34 +0200

    mend

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-import pygame
+import pygame as pg
+from constants import *
 
 def main():
-    print("Starting asteroids!")
+    print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    pg.init()
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill(color="black")
+        pg.display.flip()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
 
 if __name__ == "__main__":
     main()
